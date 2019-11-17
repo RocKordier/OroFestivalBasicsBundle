350ab7f689998d7a95b188e2bd4c79b299ac67f1
Eric Hertwig <eric@ehdev.de>
2019-11-17 21:08:27 +0100

    Easier service definition

--- src/ApiManager/FestivalAccountApiManager.php
+++ src/ApiManager/FestivalAccountApiManager.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace EHDev\FestivalBasicsBundle\ApiManager;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use EHDev\FestivalBasicsBundle\Entity\FestivalAccount;
 use Oro\Bundle\SoapBundle\Entity\Manager\ApiEntityManager;
 
 class FestivalAccountApiManager extends ApiEntityManager
 {
-    public function __construct($class, ObjectManager $om)
+    public function __construct(ObjectManager $om)
     {
-        parent::__construct($class, $om);
+        parent::__construct(FestivalAccount::class, $om);
     }
 }
